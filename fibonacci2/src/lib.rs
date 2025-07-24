416d407b1792b9180f34ab469d6dbc7a96966dfa
aelabsi <zzanass13@gmail.com>
2025-07-24 12:45:08 +0100

    fib

--- fibonacci2/src/lib.rs
+++ fibonacci2/src/lib.rs
@@ -1,4 +1,10 @@
 pub fn fibonacci(n: u32) -> u32 {
+      if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
     let mut res=0;
     let mut a = 0;
     let mut b = 1;
