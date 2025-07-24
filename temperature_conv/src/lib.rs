5a561cff9831dea3f8ac17d2bb0e0d92701dc3f2
aelabsi <zzanass13@gmail.com>
2025-07-24 10:12:28 +0100

    temp

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
 
     let mut res;
-    res = (f-32.0) / 9.0/5.0;
+    res = (f-32.0) / (9.0/5.0);
     res
 
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
      let mut res;
-    res = (c*9.0/5.0) +32.0;
+    res = (c*(9.0/5.0)) +32.0;
     res
 }
