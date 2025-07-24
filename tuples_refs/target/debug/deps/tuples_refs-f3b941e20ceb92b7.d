109e4f864920e98efcd82acf6d5e2be9001cfb5b
aelabsi <zzanass13@gmail.com>
2025-07-24 16:09:23 +0100

    tuples

--- tuples_refs/target/debug/deps/tuples_refs-f3b941e20ceb92b7.d
+++
@@ -1,7 +0,0 @@
-/home/aelabsi/Desktop/piscine-rust/tuples_refs/target/debug/deps/libtuples_refs-f3b941e20ceb92b7.rmeta: src/lib.rs
-
-/home/aelabsi/Desktop/piscine-rust/tuples_refs/target/debug/deps/libtuples_refs-f3b941e20ceb92b7.rlib: src/lib.rs
-
-/home/aelabsi/Desktop/piscine-rust/tuples_refs/target/debug/deps/tuples_refs-f3b941e20ceb92b7.d: src/lib.rs
-
-src/lib.rs:
--- tuples_refs/target/debug/libtuples_refs.d
+++
@@ -1,1 +0,0 @@
-/home/aelabsi/Desktop/piscine-rust/tuples_refs/target/debug/libtuples_refs.rlib: /home/aelabsi/Desktop/piscine-rust/tuples_refs/src/lib.rs
