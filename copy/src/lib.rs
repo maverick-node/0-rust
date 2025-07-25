838aac5bd0a43100b598b534d3e6e228c56f9694
aelabsi <zzanass13@gmail.com>
2025-07-25 14:33:02 +0100

    copy

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
     let de = c as f64;
     let exp = de.exp();
-    let log = de.ln();
+    let log =  de.abs().ln(); 
 
     (c, exp, log)
 }
@@ -24,7 +24,7 @@ pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
     let mut res: Vec<f64>= vec![];
     for i in &b {
         let num = *i as f64;
-            let log = num.ln();
+            let log =  num.abs().ln(); 
        res.push(log)
     }
     (b,res)
--- copy/src/main.rs
+++ copy/src/main.rs
@@ -1,9 +1,9 @@
 use copy::*;
 
 fn main() {
-    let a = "1 2 4 5 6".to_owned();
-    let b = vec![1, 2, 4, 5];
-    let c = 0;
+    let a = "1 2 4 5 -6".to_owned();
+    let b = vec![1, 2, 4, -5];
+    let c = -1;
 
   println!("{:?}", nbr_function(c));
     println!("{:?}", vec_function(b));
