160d9583fef1653f5d1114cf9165577b170484af
aelabsi <zzanass13@gmail.com>
2025-07-26 17:36:33 +0100

    card_deck

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,68 @@
+use rand::Rng;
+#[derive(Debug,PartialEq)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade ,
+    Club,
+}
+#[derive(Debug,PartialEq)]
+pub enum Rank {
+    Ace, 
+    King, 
+    Queen,
+    Jack,
+    Number(u8),
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        let mut rng = rand::thread_rng();
+        match rng.gen_range(1..=4) {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+        }
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid suit value: {}", value),
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let mut rng = rand::thread_rng();
+        let val = rng.gen_range(1..=13);
+        Rank::translate(val)
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            _ => Rank::King,
+        }
+    }
+}
+#[derive(Debug)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    if card.suit == Suit::Spade && card.rank == Rank::Ace {
+        return true
+    }
+    false
+}
