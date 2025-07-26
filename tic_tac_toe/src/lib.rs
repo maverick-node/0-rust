a606cfd21625e1b2fb8eef77e14e1f2d2e60dfa3
aelabsi <zzanass13@gmail.com>
2025-07-26 09:27:21 +0100

    tic-tac

---
+++ tic_tac_toe/src/lib.rs
@@ -0,0 +1,39 @@
+pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
+    
+    if vertical('O', table) || horizontal('O', table) || diagonal('O', table) {
+        return "Player O won".to_string();
+    }
+    if vertical('X', table) || horizontal('X', table) || diagonal('X', table) {
+        return "Player X won".to_string();
+    }
+    "tie".to_string()
+}
+
+pub fn diagonal(player: char, table: [[char; 3]; 3]) -> bool {
+    if table[0][0] == player && table[1][1] == player && table[2][2] == player {
+        return true;
+    }
+
+    if table[0][2] == player && table[1][1] == player && table[2][0] == player {
+        return true;
+    }
+
+    false
+}
+
+pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
+    for i in 0..3 {
+        if table[i][0] == player && table[i][1] == player && table[i][2] == player {
+            return true;
+        }
+    }
+    false
+}
+pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
+    for i in 0..3 {
+        if table[0][i] == player && table[1][i] == player && table[2][i] == player {
+            return true;
+        }
+    }
+    false
+}
