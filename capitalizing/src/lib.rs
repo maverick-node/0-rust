6cab4c0d253473c633a057a97fa4219f8c7f5dcf
aelabsi <zzanass13@gmail.com>
2025-07-27 11:18:20 +0100

    cap

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -20,16 +20,15 @@ pub fn title_case(input: &str) -> String {
     res = res.to_lowercase();
     let mut count = 0;
     for (i, c) in res.chars().enumerate() {
-        if count == 0 {
-            let upper = c.to_ascii_uppercase();
-
-            res1.push(upper);
-            count += 1;
+           if c == ' ' ||  c == '\t' {
+            res1.push(c);
+            count = 0;
             continue;
         }
-        if c == ' ' {
-            res1.push(' ');
-            count = 0;
+        if count == 0 {
+            let upper = c.to_ascii_uppercase();
+            res1.push(upper);
+            count += 1;
             continue;
         }
         res1.push(c);
