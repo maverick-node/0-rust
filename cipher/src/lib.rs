3b6c55b0e3db6b6fa2d822d4065a8662bb639b1b
aelabsi <zzanass13@gmail.com>
2025-07-31 18:26:04 +0100

    cipher

---
+++ cipher/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    expected: String,
+}
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    let mut res = String::new();
+    for i in original.chars() {
+        if i.is_alphabetic() {
+            if i.is_uppercase() {
+                let a = 'a' as u8;
+                let pos = (i.to_ascii_lowercase() as u8) - a + 1;
+                let final_pos = 27 - pos;
+                let cipher_char = (a + final_pos - 1) as char;
+                res.push_str(&cipher_char.to_string().to_uppercase());
+            }
+            if i.is_lowercase() {
+                let a = 'a' as u8;
+                let pos = (i.to_ascii_lowercase() as u8) - a + 1;
+                let final_pos = 27 - pos;
+                let cipher_char = (a + final_pos - 1) as char;
+                res.push_str(&cipher_char.to_string().to_lowercase());
+            }
+        } else {
+            res.push_str(&i.to_string());
+        }
+    }
+    if res == ciphered {
+        Ok(())
+    } else {
+        Err(CipherError {
+            expected: res.to_string(),
+        })
+    }
+}
