65e7407d74706202d2a1f226db09f761de4dade7
Christopher  Fremond <dev@01-edu.org>
2025-07-31 14:23:38 +0000

    Update profanity_filter/src/lib.rs

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,24 +1,29 @@
-pub fn check_ms(message: &str) -> Result<&str, &str>{
+pub fn check_ms(message: &str) -> Result<&str, &str> {
     let mut ve: Vec<String> = Vec::new();
 
-    for i in message.split_whitespace(){
+    for i in message.split_whitespace() {
         ve.push(i.to_string());
     }
-    if ve.contains(&("stupid").to_string()){
-      
-
+    if ve.contains(&"stupid".to_string()) {
+        return Err("ERROR: illegal");
     }
 
+    if ve.is_empty() {
+        return Err("ERROR: illegal");
+    }
 
-
+    Ok(message)
 }
 
-pub struct Message{
- msg: String,
+pub struct Message {
+    msg: String,
 }
 
-impl Message{
-   pub fn new(st: String, st2: String)->String{
-        st+ " "+ &st2
+impl Message {
+    pub fn new(st: String, st2: String) -> String {
+        if st == "" || st2 == "" {
+            return "".to_string();
+        }
+        st + " " + &st2
     }
 }
