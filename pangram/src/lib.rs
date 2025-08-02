06cfda0f406169e7889505529c5125ce3644a4c9
aelabsi <zzanass13@gmail.com>
2025-08-02 11:25:23 +0100

    pangram

---
+++ pangram/src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+pub fn is_pangram(s: &str) -> bool {
+    let mut count = 0;
+    let mut map = HashMap::new();
+    for i in s.chars() {
+        map.insert(i,0);
+    }
+    if map.len() >= 28 {
+        return true;
+    }
+    println!("{}", map.len());
+    false
+}
