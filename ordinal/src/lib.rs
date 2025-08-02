4f0d6181f7559d80297fd0d9ba0a36e9a2e8f7dd
aelabsi <zzanass13@gmail.com>
2025-08-02 10:27:59 +0100

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,11 +1,11 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let mut st = x.to_string();
 
-    if st.ends_with("1") {
+    if st == "1"{
         st.push_str("st");
-    }else if st.ends_with("2") {
+    }else if st == "2" {
         st.push_str("nd");
-    }else if st.ends_with("3") {
+    }else if st=="3" {
         st.push_str("rd");
     } else {
         st.push_str("th");
