cef49a63e954ce9baaea8d93ff2dd25c0acf3c64
aelabsi <zzanass13@gmail.com>
2025-08-02 10:56:45 +0100

    ord

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,14 +1,17 @@
 pub fn num_to_ordinal(x: u32) -> String {
-    let mut st = x.to_string();
-
-    if st == "1"{
-        st.push_str("st");
-    }else if st == "2" {
-        st.push_str("nd");
-    }else if st=="3" {
-        st.push_str("rd");
-    } else {
-        st.push_str("th");
+    let mut res = String::new();
+      if x % 100 == 11 || x % 100 == 12 || x % 100 == 13 {
+        return x.to_string() + "th";
     }
-    st
+    let suffix = if x % 10 == 1 {
+        "st"
+    } else if x % 10 == 2 {
+        "nd"
+    } else if x % 10 == 3 {
+        "rd"
+    } else {
+        "th"
+    };
+
+    x.to_string() + suffix
 }
