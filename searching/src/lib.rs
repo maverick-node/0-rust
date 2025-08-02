354cbcfd9a4fd34f97fb7539ae641ba3b3a5840c
aelabsi <zzanass13@gmail.com>
2025-08-02 10:02:21 +0100

    searching

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,10 +1,18 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    let mut res = array.len()-1;
-    for i in array.iter().rev(){
-        if key == *i{
-            return Some(res)
+    let mut res = array.len() - 1;
+    loop {
+        for i in array.iter().rev() {
+            if res <= 0 {
+                return None
+            }
+            if key == *i {
+                return Some(res);
+            }
+            res -= 1;
         }
-        res-=1;
+
+        
     }
-    Some(res)
+
+   
 }
