81ce6c46eb23e81c11b6252ac3d203b511bf9c94
aelabsi <zzanass13@gmail.com>
2025-08-02 11:26:48 +0100

    v2

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,11 +3,13 @@ pub fn is_pangram(s: &str) -> bool {
     let mut count = 0;
     let mut map = HashMap::new();
     for i in s.chars() {
-        map.insert(i,0);
+        if i.is_ascii_alphabetic(){
+
+            map.insert(i,0);
+        }
     }
-    if map.len() >= 28 {
+    if map.len() == 26 {
         return true;
     }
-    println!("{}", map.len());
     false
 }
