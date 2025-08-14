42e2512c6f6edfef6123dc097bce603e050a2c2b
aelabsi <zzanass13@gmail.com>
2025-08-14 14:32:22 +0100

    matrix

---
+++ matrix/src/lib.rs
@@ -0,0 +1,23 @@
+#[derive(Debug,Clone)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+use lalgebra_scalar::*;
+impl<T> Matrix<T>
+where
+    T: Scalar<Item = T> + Clone,{
+
+	pub fn new() -> Matrix<T> {
+		Self(vec![vec![]])
+	}
+
+	pub fn zero(row: usize, col: usize) -> Matrix<T> {
+		Self(vec![vec![T::zero();col];row])
+	}
+
+	pub fn identity(n: usize) -> Matrix<T> {
+		let mut mat = vec![vec![T::zero(); n]; n];
+        for i in 0..n {
+            mat[i][i] = T::one();
+        }
+        Self(mat)
+	}
+}
